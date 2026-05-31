88ca55b89a50de0c8f85abd88c7ae67a3ed6cafb
mklopocki <poczta@mklopocki.biz>
2026-05-31 12:53:46 +0200

    Update unifiswitch.rb
    edit regexp logic to grab console prompt, take into block commands to run in post login and pre logout actions.

--- lib/oxidized/model/unifiswitch.rb
+++ lib/oxidized/model/unifiswitch.rb
@@ -1,4 +1,5 @@
 class Unifiswitch < Oxidized::Model
+  using Refinements
 
   comment '!'
 
@@ -6,22 +7,30 @@ class Unifiswitch < Oxidized::Model
   # 1. Initial shell:  NAME.0.0.0#
   # 2. Unprivileged:   (UBNT) >
   # 3. Privileged:     (UBNT) #
-  prompt /^(?:\([\w\s.-]+\)\s[>#]|[\w\.-]+#)\s*$/
+  prompt /^(?:[\w.-]+#|\(UBNT\)\s[>#])\s*$/
 
   # The command used to gather the configuration
   cmd 'show running-config' do |cfg|
-    cfg.gsub! /^!System Up Time\s+.*$/, '\\1 <stripped>'
-    cfg
+    cfg.gsub! /(!System Up Time\s+-).*/, '\\1 <stripped>'
+    cfg.each_line.to_a[4..-4].join
   end
 
-  cfg :ssh, :telnet do
+  cfg :ssh do
     # Commands to run immediately after successful authentication
-    post_login 'cli'
-    post_login 'enable'
-    post_login 'terminal length 0'
-    
+    post_login do
+      sleep 2
+      cmd 'cli'
+      cmd 'enable'
+      cmd 'terminal length 0'
+    end
+
     # Commands to gracefully exit the sessions
-    pre_logout 'exit' # Exits the (UBNT) CLI
-    pre_logout 'exit' # Exits the initial shell
+    pre_logout do
+      cmd 'exit' # Exits the (UBNT) #
+      sleep 0.5
+       cmd 'exit' # Exits the (UBNT) >
+      sleep 0.5
+      cmd 'exit' # Exits the initial shell
+    end
   end
 end
